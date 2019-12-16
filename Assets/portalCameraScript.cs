f690465db4fbfbcbe900b3f79f1a0017cd0b9128
a-birb <56260898+a-birb@users.noreply.github.com>
2019-12-16 08:43:47 -0500

    Initial commit

---
+++ Assets/portalCameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class portalCameraScript : MonoBehaviour
+{
+    public Transform player;
+    public Transform pointB;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angleDegrees = Mathf.Atan2(gameObject.transform.position.z - player.position.z, gameObject.transform.position.x - player.position.x) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,angleDegrees,transform.eulerAngles.z);
+    }
+
+}
